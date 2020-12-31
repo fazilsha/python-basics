124ad2809264f158876090d3e97e6073c3f24208
fazilsha <fazilsha1197@gmail.com>
2020-12-31 15:36:45 +0530

    second commit

--- listbasics.py
+++ listbasics.py
@@ -2,3 +2,14 @@ a=[1,2,3,4,5]
 for i in a:
     print(i)
 print(type(a))
+
+#append
+a.append(6)
+print(a)
+#updating
+print("updating 0 at 0th index")
+a[0]=0
+print(a)
+print("Removing element at 2nd index")
+a.remove(a[2])
+print(a)
