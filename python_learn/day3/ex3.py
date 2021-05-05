9e3307a9814c45a36dd9fe9fecf5331b2fb70980
fazilsha <fazilsha1197@gmail.com>
2021-05-06 00:50:00 +0530

    fizz buzz

--- python_learn/day3/ex3.py
+++ python_learn/day3/ex3.py
@@ -1,4 +1,20 @@
 '''
 Implement FizzBuzz (look it up if you're not familiar with it) in Python
 '''
+fizzbuzz=int(input("Enter the number to check FizzBuzz: "))
+if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+    print("fizzbuzz")
+elif fizzbuzz % 3 == 0:
+    print("fizz")
+elif fizzbuzz % 5 == 0:
+    print("buzz")
+print(fizzbuzz)
 
+'''
+Enter the number to check FizzBuzz: 15                        fizzbuzz
+15
+Enter the number to check FizzBuzz: 5                         buzz
+5                                                           
+Enter the number to check FizzBuzz: 17                        17
+
+'''
